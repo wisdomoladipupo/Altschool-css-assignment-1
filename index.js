c2940c88eb4c596249ca8acb8a14b03701f2d4e4
wisdom oladipupo <wisbel71@gmail.com>
2024-11-14 21:00:03 +0100

    fisrt submission final  commit

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const burger = document.getElementById("svg");
-const navlist = document.querySelector(".navList");
+document.addEventListener("DOMContentLoaded",() => {
+  const burger = document.getElementById("svg")
 
-burger.addEventListener("click", () => {
-    if (navlist.style.display === "none" || navlist.style.display === "") {
-      navlist.style.display = "block";
-    } else {
-      navlist.style.display = "none";
-    }
-});
+  const   navlist = document.querySelector(".navList")
+  
+  burger.addEventListener("click", (e) => {
+      e.stopPropagation()
+      if(navlist.style.display === "none" || navlist.style.display === ""){
+          navlist.style.display = "block"
+      } else {
+          navlist.style.display = "none"
+      }
+  })
+  
+  document.addEventListener("click",(e) => {
+      if (navlist.style.display === "block"){
+          navlist.style.display = "none"
+      }
+  })
+  
+  navlist.addEventListener("click",(e) => {
+      e.stopPropagation()
+  })
+})
+
